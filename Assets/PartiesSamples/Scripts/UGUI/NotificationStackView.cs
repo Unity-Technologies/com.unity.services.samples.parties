bd0605ef7b2e874d5614f1172ca2c3547f60fa27
UnityJacob <jacob.lorentzen@unity3d.com>
2023-02-17 12:11:58 -0800

    fix : Notifications scale up now.

--- Assets/PartiesSamples/Scripts/UGUI/NotificationStackView.cs
+++ Assets/PartiesSamples/Scripts/UGUI/NotificationStackView.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Unity.Services.Samples.Parties
@@ -9,10 +7,19 @@ namespace Unity.Services.Samples.Parties
         [SerializeField] NotificationView m_NotificationView;
         [SerializeField] Transform m_StackParent;
 
+        //Performance Note : Should probably be pooled
         public void CreateNotification(float lifeTime, string playerName, string notificationContent)
         {
             var notificationInstance = Instantiate(m_NotificationView, m_StackParent);
             notificationInstance.Init(lifeTime, playerName, notificationContent);
+        }
+
+        /// <summary>
+        /// Throw this into a UI button to quickly test how it looks
+        /// </summary>
+        public void TestNotify()
+        {
+            CreateNotification(1,"Unity", "Test Test Test Test");
         }
     }
 }
